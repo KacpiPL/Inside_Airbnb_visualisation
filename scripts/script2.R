fd03bd966b7f7737a21e49f360c5a1c2b44955d0
Melkri <michal.bryzik13@gmail.com>
2024-01-15 21:14:18 +0100

    Add Loading Shapefiles, creating maps

--- scripts/script2.R
+++ scripts/script2.R
@@ -2,7 +2,9 @@ library(dplyr)
 library(tidyr)
 library(stringr)
 library(ggplot2)
-
+library(leaflet)
+library(geojsonio)
+library(sf)
 
 rm(list=ls())
 
@@ -15,7 +17,12 @@ num_obs_city_with_nas <- df %>%
   summarise(n())
 
 num_nas <- as.data.frame(colSums(is.na(df)))
+
+df <- df %>%
+  mutate(neighbourhood = ifelse(City == "Berlin", neighbourhood_group, neighbourhood)) %>%
+  mutate(neighbourhood = ifelse(City == "Paris" & neighbourhood == "Entrepôt", "Enclos-St-Laurent", neighbourhood))
 # delete column with the most NAs and unnecessary
+
 df <- subset(df, select = -neighbourhood_group)
 # delete reviews per month column
 df <- subset(df, select= -reviews_per_month)
@@ -112,6 +119,62 @@ hist(df_Paris_z$Price_EUR)
 hist(df_Berlin_z$Price_EUR)
 hist(df_London_z$Price_EUR)
 
+##### Load the Shapefiles #####
+
+# Load the shapefiles
+berlin_sp <- geojson_read("./data/berlin.geojson", what = 'sp')
+london_sp <- geojson_read("./data/london.geojson", what = 'sp')
+paris_sp <- geojson_read("./data/paris.geojson", what = 'sp')
+
+# Shapefile to Simple Feature objects
+paris_sp_sf <- st_as_sf(paris_sp)
+london_sp_sf <- st_as_sf(london_sp)
+berlin_sp_sf <- st_as_sf(berlin_sp)
+
+##### Map Chart #####
+
+# Join the simple feature objects with dfs
+paris_data <- left_join(paris_sp_sf, df %>% filter(City == "Paris") %>% group_by(neighbourhood) %>% summarise(Avg_Rating = mean(Rating)), by = c("name" = "neighbourhood"))
+london_data <- left_join(london_sp_sf, df %>% filter(City == "London") %>% group_by(neighbourhood) %>% summarise(Avg_Rating = mean(Rating)), by = c("name" = "neighbourhood"))
+berlin_data <- left_join(berlin_sp_sf, df %>% filter(City == "Berlin") %>% group_by(neighbourhood) %>% summarise(Avg_Rating = mean(Rating)), by = c("BEZIRKSNAM" = "neighbourhood"))
+
+# Create the maps
+ggplot() +
+  geom_sf(data = paris_data, aes(fill = Avg_Rating), color = "black") +
+  scale_fill_continuous(low = "white", high = "red", name = "Avg. Rating")
+
+ggplot() +
+  geom_sf(data = london_data, aes(fill = Avg_Rating), color = "black") +
+  scale_fill_continuous(low = "white", high = "red", name = "Avg. Rating")
+
+ggplot() +
+  geom_sf(data = berlin_data, aes(fill = Avg_Rating), color = "black") +
+  scale_fill_continuous(low = "white", high = "red", name = "Avg. Rating")
+
+##### Map Chart - Points #####
 
 
+paris_data <- df %>% filter(City == "Paris")
+london_data <- df %>% filter(City == "London")
+berlin_data <- df %>% filter(City == "Berlin")
 
+pal <- colorNumeric(palette = "viridis", domain = df$Price_EUR)
+
+# Create the maps
+leaflet(paris_data) %>%
+  addTiles() %>%
+  addCircleMarkers(~longitude, ~latitude, color = ~pal(Price_EUR), fillOpacity = 0.1, radius = 0.1) %>%
+  setView(lng = mean(paris_data$longitude), lat = mean(paris_data$latitude), zoom = 12) %>%
+  addLegend(pal = pal, values = ~Price_EUR, title = "Price")
+
+leaflet(london_data) %>%
+  addTiles() %>%
+  addCircleMarkers(~longitude, ~latitude, color = ~pal(Price_EUR), fillOpacity = 0.1, radius = 0.01) %>%
+  setView(lng = mean(london_data$longitude), lat = mean(london_data$latitude), zoom = 10) %>%
+  addLegend(pal = pal, values = ~Price_EUR, title = "Price")
+
+leaflet(berlin_data) %>%
+  addTiles() %>%
+  addCircleMarkers(~longitude, ~latitude, color = ~pal(Price_EUR), fillOpacity = 0.1, radius = 0.1) %>%
+  setView(lng = mean(berlin_data$longitude), lat = mean(berlin_data$latitude), zoom = 12) %>%
+  addLegend(pal = pal, values = ~Price_EUR, title = "Price")
