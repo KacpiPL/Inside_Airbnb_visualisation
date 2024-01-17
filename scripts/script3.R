d87871b027663b01b3aa4ac53c61ae9db8c130dc
Kacper <kacpergruca@gmail.com>
2024-01-17 23:03:18 +0100

    Add line graphs, z score method to 1 column

--- scripts/script3.R
+++ scripts/script3.R
@@ -9,9 +9,13 @@ library(reshape2)
 library(hrbrthemes)
 library(viridis)
 
-
 rm(list=ls())
-df <- read.csv("./data/df_final.csv")
+
+df <- read.csv("./data/final_df.csv")
+df <- df[, -1]
+
+df <- df %>%
+  filter(Date == "2023-12-31")
 
 # Inspirations:
 
@@ -99,6 +103,69 @@ ggplot(df, aes(x = Rating, y = Price_EUR, size = Rating)) +
   labs(title = "Price vs. Number of Reviews by Room Type", x = "Rating", y = "Price (EUR)")
 
 
+##### Listings in time #####
+rm(list=ls())
+
+# read df from script1
+df <- read.csv("./data/df.csv")
+
+df_Berlin <- df[df$City == "Berlin",]
+df_Paris <- df[df$City == "Paris",]
+df_London <- df[df$City == "London",]
+
+delete_outliers_one_column <- function(df, threshold, col_name){
+  z_scores <- df[col_name] %>% 
+    lapply(function(x) scale(x, center = TRUE, scale = TRUE)) %>%
+    as.data.frame()
+  
+  # Create a logical index for rows to keep
+  threshold <- 3
+  rows_to_keep <- apply(z_scores, 1, function(x) all(abs(x) <= threshold))
+  
+  # Filter out outliers
+  df <- df[rows_to_keep, ]
+}
+
+# Apply function to each city
+df_Berlin_z <- delete_outliers_one_column(df_Berlin, 3, "Price_EUR")
+df_Paris_z <- delete_outliers_one_column(df_Paris, 3, "Price_EUR")
+df_London_z <- delete_outliers_one_column(df_London, 3, "Price_EUR")
+
+# Merge dfs
+df_z <- rbind(df_Berlin_z, df_Paris_z, df_London_z)
+df <- df_z
+
+rm(df_Berlin, df_Berlin_z, df_London, df_London_z, df_Paris, df_Paris_z, df_z)
+
+df_chart <- df[, c("Date", "City", "Beds", "Price_EUR")]
+df_chart <- na.omit(df_chart)
+
+# Avg price for every city in time
+df_chart_grouped <- df_chart %>%
+  group_by(Date, City) %>%
+  summarise(avg_price = round(mean(Price_EUR/Beds), 2))
+
+ggplot(data = df_chart_grouped, aes(x = Date, y = avg_price, 
+                          color = factor(City),
+                          group = factor(City))) +
+  geom_point() +
+  geom_line() +
+  theme_minimal()
+
+# Number of listings in each city
+df_chart2 <- df[, c("Date", "City")]
+df_chart2 <- na.omit(df_chart2)
+
+df_chart2_grouped <- df_chart2 %>%
+  group_by(Date, City) %>%
+  summarise(num_listings = round(n(), 2))
+
+ggplot(data = df_chart2_grouped, aes(x = Date, y = num_listings, 
+                                    color = factor(City),
+                                    group = factor(City))) +
+  geom_point() +
+  geom_line() +
+  theme_minimal()
 
 
 
